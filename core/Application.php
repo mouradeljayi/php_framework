673d95aebfb063130f9eafef92b33eeae5940624
Mourad EL Jayi <mouradeljayi98@gmail.com>
2021-08-05 13:48:35 +0100

    build views

--- core/Application.php
+++ core/Application.php
@@ -7,19 +7,20 @@ namespace app\core;
  */
 class Application
 {
-
+  public static string $ROOT_DIR;
   public Router $router;
   public Request $request;
 
-  function __construct()
+  function __construct($rootPath)
   {
+    self::$ROOT_DIR = $rootPath;
     $this->request = new Request();
     $this->router = new Router($this->request);
   }
 
   public function run()
   {
-    $this->router->resolve();
+    echo $this->router->resolve();
   }
 
 }
