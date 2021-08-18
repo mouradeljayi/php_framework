d57cf11d58f3d3ae0a71fc9489409a5e12ffda85
Mourad EL Jayi <mouradeljayi98@gmail.com>
2021-08-18 18:44:35 +0100

    create register method

--- views/register.php
+++ views/register.php
@@ -1,31 +1,23 @@
-<h1>register</h1>
+<h1 class="mt-5">Create an account</h1>
 
-<div class="container mt-5">
-  <div class="row justify-content-center">
-    <div class="col-md-6">
-      <form action="" method="post">
-        <div class="mb-3">
-          <label>Firstname</label>
-          <input type="text" name="firstname" class="form-control">
+<div class="row justify-content-center">
+  <div class="col-md-6">
+    <?php $form = \app\core\form\Form::begin('', "post") ?>
+      <div class="row">
+        <div class="col">
+          <?php echo $form->field($model, 'firstname') ?>
         </div>
-        <div class="mb-3">
-          <label>Lastname</label>
-          <input type="text" name="lastname" class="form-control">
+        <div class="col">
+          <?php echo $form->field($model, 'lastname') ?>
         </div>
-        <div class="mb-3">
-          <label>email</label>
-          <input type="email" name="email" class="form-control">
-        </div>
-        <div class="mb-3">
-          <label>password</label>
-          <input type="password" name="password" class="form-control">
-        </div>
-        <div class="mb-3">
-          <label>confirm password</label>
-          <input type="password" name="confirmPassword" class="form-control">
-        </div>
-        <button type="submit" class="btn btn-primary">register</button>
-      </form>
-    </div>
+      </div>
+        <?php echo $form->field($model, 'email') ?>
+        <?php echo $form->field($model, 'password')->passwordField() ?>
+        <?php echo $form->field($model, 'confirmPassword')->passwordField() ?>
+
+        <button type="submit" class="btn btn-primary mt-2">register</button>
+
+    <?php \app\core\form\Form::end() ?>
+
   </div>
 </div>
