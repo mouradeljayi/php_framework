84916c18e231b29667bf24d386eb82fe0cc1cb3a
Mourad EL Jayi <mouradeljayi98@gmail.com>
2021-08-25 15:49:25 +0100

    add unique rule

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -2,7 +2,7 @@
 namespace app\controllers;
 use app\core\Request;
 use app\core\Controller;
-use app\models\RegisterModel;
+use app\models\User;
 
 /**
  * Class AuthController
@@ -19,23 +19,23 @@ class AuthController extends Controller
   public function register(Request $request)
   {
 
-    $registerModel = new RegisterModel();
+    $user = new User();
 
     if ($request->isPost()) {
-      $registerModel->loadData($request->getBody());
+      $user->loadData($request->getBody());
 
-      if($registerModel->validate() && $registerModel->register()) {
+      if($user->validate() && $user->save()) {
         return "Success";
       }
-      
+
       return $this->render('register', [
-        'model' => $registerModel
+        'model' => $user
       ]);
     }
 
     $this->setLayout('auth');
     return $this->render('register', [
-      'model' => $registerModel
+      'model' => $user
     ]);
   }
 }
